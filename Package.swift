4ac99c5158ae7997bead7437b041eb09e8b85758
Vishnu <ivishbishnoi@gmail.com>
2024-05-13 17:23:35 +0530

    Update model version

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["JoyfillAPIService"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/joyfill/JoyfillModel", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/joyfill/JoyfillModel", .upToNextMajor(from: "1.0.1")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
